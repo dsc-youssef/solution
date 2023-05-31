85efe23cae278dcb9182a75bddb5b625d7029c5e
youssef <youssef_bebawy@hotmail.com>
2023-06-01 02:08:52 +0300

    create database actions and fix customer table

--- database/migrations/2023_05_28_165022_create_customers_table.php
+++ database/migrations/2023_05_28_165022_create_customers_table.php
@@ -4,32 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 100)->nullable(false);
-            $table->integer('projects')->nullable(false)->default(0);
-            $table->bigInteger('spent')->nullable(false)->default(0);
-            $table->boolean('veifed')->nullable(false)->default(false);
-            $table->string('image', 255)->nullable(false)->default("");
-            $table->string('phone', 50)->nullable(false);
-            $table->string('email', 100)->nullable(false);
-            $table->string('country', 50)->nullable(false);
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('customers', function (Blueprint $table) {
+      $table->id();
+      $table->integer('projects')->nullable(false)->default(0);
+      $table->bigInteger('spent')->nullable(false)->default(0);
+      $table->boolean('verified')->nullable(false)->default(false);
+      $table->unsignedBigInteger("user_id");
+      $table->foreign("user_id")->references("id")->on("users")->cascadeOnDelete();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('customers');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('customers');
+  }
 };
--- database/migrations/2023_05_28_165559_create_web_sales_table.php
+++ database/migrations/2023_05_28_165559_create_web_sales_table.php
@@ -4,30 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('web_sales', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger("customer_id");
-            $table->foreign("customer_id")->references("id")->on("customers")->onDelete('cascade');
-            $table->unsignedBigInteger("product_id");
-            $table->foreign("product_id")->references("id")->on("products")->onDelete('cascade');
-            $table->unsignedBigInteger("user_id");
-            $table->foreign("user_id")->references("id")->on("users")->onDelete('cascade');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('web_sales', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger("customer_id");
+      $table->foreign("customer_id")->references("id")->on("customers")->onDelete('cascade');
+      $table->unsignedBigInteger("product_id");
+      $table->foreign("product_id")->references("id")->on("products")->onDelete('cascade');
+      $table->boolean("accepted")->default(false);
+      $table->decimal('paid', 13, 2)->nullable(false);
+      $table->decimal('remaining', 13, 2)->nullable(false);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('web_sales');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('web_sales');
+  }
 };
