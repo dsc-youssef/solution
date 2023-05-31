20e6a7608fbdc23381bb78faf2c0b323bbccad03
youssef <youssef_bebawy@hotmail.com>
2023-06-01 00:45:23 +0300

    create navbar

--- view/src/components/navbar/index.tsx
+++ view/src/components/navbar/index.tsx
@@ -1,11 +1,22 @@
 // Dependencies
 import { FC } from "react";
 
+// Bootstrap Components
+import { Container } from "react-bootstrap";
+
+// Navbar Components
+import UserButtons from "./components/UserButtons";
+
 
 const MainNavbar: FC = () => {
   return (
     <nav className="main-navbar">
-      {/* Add Our Code Here ! */}
+      <Container fluid>
+        <div>
+          <i className="fal fa-bars-staggered cursor-pointer" />
+        </div>
+        <UserButtons />
+      </Container>
     </nav>
   )
 }
